b1f981e6fb8c8ebbea71836c76163c10237e49d4
user <vanderson.dev1@gmail.com>
2023-07-05 21:02:23 -0300

    fourth commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,63 @@
 
 
-let clients = [
+let clients = [{
+    nome: 'Teste1',
+    CPF: 99999999911,
+    RG: 1301300001,
+    Telefone: 77999999999,
+    Endereço: 'rua sei la o que',
+    Numero: 120,
+    Bairro: 'new city',
+    Sexualidade: 'masculino',
+    plano: '250mb'
+},
+{
+    nome: 'Teste2',
+    CPF: 99999999911,
+    RG: 1301300001,
+    Telefone: 77999999999,
+    Endereço: 'rua sei la o que',
+    Numero: 120,
+    Bairro: 'new city',
+    Sexualidade: 'masculino',
+    plano: '250mb'
+},
+{
+    nome: 'Teste3',
+    CPF: 99999999911,
+    RG: 1301300001,
+    Telefone: 77999999999,
+    Endereço: 'rua sei la o que',
+    Numero: 120,
+    Bairro: 'new city',
+    Sexualidade: 'masculino',
+    plano: '250mb'
+},
+{
+    nome: 'Teste32',
+    CPF: 99999999911,
+    RG: 1301300001,
+    Telefone: 77999999999,
+    Endereço: 'rua sei la o que',
+    Numero: 120,
+    Bairro: 'new city',
+    Sexualidade: 'masculino',
+    plano: '250mb'
+}
+
+,
+{
+    nome: 'Teste31',
+    CPF: 99999999911,
+    RG: 1301300001,
+    Telefone: 77999999999,
+    Endereço: 'rua sei la o que',
+    Numero: 120,
+    Bairro: 'new city',
+    Sexualidade: 'masculino',
+    plano: '250mb'
+}
+
 
 ]
 
@@ -49,11 +106,71 @@ osBtn.addEventListener('click', (e)=>{
 const search = document.querySelector("#search")
 const render = document.querySelector("div.show-client")
 
-function searchInKeyUp(){
-    console.log('yes')
+function searchInKeyUp(event){
+    let searched = event.target.value
+    console.log(searched)
+
+    let clientFound = filterSearch(searched)
+    console.log(clientFound)
+
+    let list = ' '
+
+    if(searched.length<=0){
+        /* setTimeout(()=>{
+            alertBox.style.display = 'none'
+        }, 3000)
+        alertBox.style.display = 'flex'
+        alertBox.innerHTML = 'Digite o nome de um cliente para mostrar o cadastro no dashboard.'
+        dashboard.style.display = 'flex' */
+    }else{
+
+        //desativando os botoes da sidebar
+        osBtn.classList.remove("active")
+        dashboardBtn.classList.remove("active")
+        cadastrosBtn.classList.remove("active")
+        contasBtn.classList.remove("active")
+        /* dashboard.style.display = 'none' */
+        clientFound.forEach((client, index)=>{
+            list += `
+                <div class='show-multiples'>
+                    <div class='edit-client' onclick='editClient()'><span class="material-symbols-sharp">
+                    edit
+                    </span></div>
+                    <div class='atributos'>
+                        <div class='client-atrib'>
+                            <h3>Cliente: </h3>
+                            <p> ${client.nome}</p>
+                        </div>
+                        <div class='client-atrib'>
+                            <h3>CPF/CNPJ: </h3>
+                            <p> ${client.CPF}</p>
+                        </div>
+                        <div class='client-atrib'>
+                            <h3>RG/IE: </h3>
+                            <p> ${client.RG}</p>
+                        </div>
+                        <div class='client-atrib'>
+                            <h3>Telefone: </h3>
+                            <p> ${client.Telefone}</p>
+                        </div>
+                        <div class='client-atrib'>
+                            <h3>Plano: </h3>
+                            <p> ${client.plano}</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        })
+    }
+    render.style.display = 'flex'
+    render.innerHTML = list
 }
 
 
 search.addEventListener('keyup', _.debounce(searchInKeyUp, 400))
 
-
+function filterSearch(searched){
+    return clients.filter((client =>{
+        return client.nome.toLowerCase().includes(searched.toLowerCase())
+    }))
+}
